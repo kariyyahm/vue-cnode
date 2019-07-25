501c22f0dcdf4af0a2c4c2ad1754b0929ab5bfa9
kari <mr_hm@outlook.com>
2019-07-25 20:00:08 +0800

    更新

---
+++ my-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <div class="main">
+      <router-view name="slidebar"></router-view>
+      <router-view name="main"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+export default {
+  name: "App",
+  components: {
+    Header,
+    PostList
+  }
+};
+</script>
+
+<style>
+body{
+  background-color: #e1e1e1;
+}
+.main{
+  max-width: 1400px;
+  margin: 0 auto;
+}
+</style>
+
